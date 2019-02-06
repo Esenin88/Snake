12f8126a6190a766dd4cde0e535e193c9a4905e5
Esenin88 <esenin_developer@mail.ru>
2019-02-06 20:00:27 +0300

    инкапсуляция

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,7 +10,15 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            
+            Point p1 = new Point(1, 3, '*');
+
+            p1.Drow();
+
+            Point p2 = new Point(4, 5, '#');
+
+            p2.Drow();
+            Console.ReadLine();
         }
+
     }
 }
