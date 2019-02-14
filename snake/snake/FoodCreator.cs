f61aecbb8d45704ff5edfe963963f82aff5e1564
Esenin88 <esenin_developer@mail.ru>
2019-02-14 19:56:15 +0300

    создаем новый класс создания еды

---
+++ snake/snake/FoodCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class FoodCreator
+    {
+        int MapHeigth;
+        int MapWidth;
+        char sym;
+
+        Random random = new Random();
+        public FoodCreator(int MapWidth, int MapHeigth, char sym)
+        {
+            this.MapHeigth = MapHeigth;
+            this.MapWidth = MapWidth;
+            this.sym = sym;
+        }
+        public Point CreateFood()
+        {
+            int x = random.Next(2, MapWidth - 2);
+            int y = random.Next(2, MapHeigth - 2);
+            return new Point(x, y, sym);
+        }
+            
+
+    }
+}
